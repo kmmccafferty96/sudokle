e7d86ac2fecebc06dad62a63a1f3a5fb9f03dff8
Keegan McCafferty <kmmccafferty96@gmail.com>
2022-04-27 23:37:20 -0400

    chore: add husky, prettier, eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sud-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor() {
+    console.log(
+      'asdjfkasjd lfkjasdkfalksdjfkasj dfklasj kfjskd jflajslkfjlkasjflkjaskldf klsjfjasfkj aslkfj klsjf lksjdf klsjlfkjslfklasdf asfasjfjs',
+    );
+  }
 }
